94b5982901bc09bad54276f6abbc6111a3046e08
Salimmadeit <salimadebola@gmail.com>
2026-09-18 01:13:27 +0100

    project completed

--- task/billsplitter.py
+++ task/billsplitter.py
@@ -1,10 +1,46 @@
 # write your code here
-num_of_attendees = int(input("Enter the number of friends joining (including you):"))
+import random
 
-attendees = {}
+num_of_attendees: int = int(input("Enter the number of friends joining (including you):\n> "))
+attendees: dict = {}
+personal_bill_value: int | float = 0
 
-for i in range(num_of_attendees):
-    current_attendee = input("")
-    attendees[current_attendee] = 0
+if num_of_attendees <= 0:
+    print("\nNo one is joining for the party")
+else:
 
-print(attendees)
+    print("")
+    print("Enter the name of every friend (including you), each on a new line:")
+    for i in range(num_of_attendees):
+        current_attendee: str = input("> ")
+        attendees[current_attendee] = personal_bill_value
+
+    total_bill_value: int = int(input("Enter the total bill value:\n> "))
+    personal_bill_value += round((total_bill_value / num_of_attendees), 2)
+
+    # assigned the average bill evenly to everyone
+    for name, bill_value in attendees.items():
+        attendees[name] = personal_bill_value
+
+    # who_is_lucky feature
+    who_is_lucky = input('Do you want to use the "Who is lucky?" feature? Write Yes / No:\n> ')
+    luck = False
+    lucky_one = random.choice([key for key in attendees.keys()])
+    if who_is_lucky == "Yes" or who_is_lucky == "yes":
+        luck = True
+        print(f"{lucky_one} is the lucky one!")
+        lucky_ones_bill = ( attendees[lucky_one] / ( num_of_attendees - 1 ) )
+        for key in attendees.keys():
+            attendees[key] += lucky_ones_bill
+        attendees[lucky_one] = 0
+
+        print(attendees)
+
+    else:
+        luck = False
+        print("No one is going to be lucky")
+        print(attendees)
+
+
+
+
--- task/test/tests.py
+++ task/test/tests.py
@@ -1,7 +1,8 @@
 from hstest import CheckResult, StageTest, dynamic_test, TestedProgram
-import ast
+import ast, math
 import re
 
+END_RESULT = "No one is going to be lucky"
 INVALID_RESULT = "No one is joining for the party"
 
 
@@ -24,21 +25,37 @@ class BillSplitterTest(StageTest):
         return CheckResult.correct()
 
     test_data = [
-        [5, ["Marc", "Jem", "Monica", "Anna", "Jason"]],
-        [3, ["Jake", "Sam", "Irina"]],
-        [2, ["Jake", "Sam"]],
+        [5, ["Marc", "Jem", "Monica", "Anna", "Jason"], 100, True],
+        [3, ["Jake", "Sam", "Irina"], 109, False],
+        [2, ["Jake", "Sam"], 109, False],
     ]
 
     @dynamic_test(data=test_data)
-    def test(self, num, friends):
+    def test(self, num, friends, total, luckypick):
         pr = TestedProgram()
-        output = pr.start()
-        for inp in [str(num)] + friends:
-            output = pr.execute(inp)
+        pr.start()
+        for inp in [str(num)] + friends + [str(total)]:
+            pr.execute(inp)
+        output = pr.execute(str('Yes' if luckypick else 'No'))
+        luckyname = ""
+        if luckypick:
+            luckyname = output.strip().split(' ')[0].lower()
+            if luckyname not in [n.lower() for n in friends]:
+                return CheckResult.wrong(
+                    "Expected output is a random name from dictionary keys, but we got something else")
+        elif (re.sub(r"\s", '', END_RESULT.strip().lower())
+              not in re.sub(r"\s", '', output.strip().lower())):
+            return CheckResult.wrong('When a "No" option is provided as an input for a lucky feature '
+                                     f'your program should output "{END_RESULT}" string')
+        match = re.search(r'\{.+\}', output)
+        if match:
+            output = match.group()
+        else:
+            return CheckResult.wrong('Please check your last output, it should be a dictionary')
         try:
             user_dict = ast.literal_eval(output.lower())
         except ValueError:
-            return CheckResult.wrong('Please check your output, it should be a dictionary')
+            return CheckResult.wrong('Please check your last output, it should be a dictionary')
         except IndentationError:
             return CheckResult.wrong('There should not be any leading whitespace before your last output')
         except Exception:
@@ -46,21 +63,31 @@ class BillSplitterTest(StageTest):
                                      'Make sure you print the dictionary like in examples!\n'
                                      f'Found dict: \n{output}')
         if not isinstance(user_dict, dict):
-            return CheckResult.wrong('Please use Dictionary data structure to store user input')
+            return CheckResult.wrong('Please check your last output, it should be a dictionary')
         elif len(user_dict) != num:
             return CheckResult.wrong('Please check if you have added all your friends to dictionary '
                                      'after taking an user input')
-
         try:
             bill_list = list(user_dict.values())
             bill = sum(bill_list)
         except TypeError:
             return CheckResult.wrong("Dictionary values should be of integer type")
 
-        if not all([v == 0 for v in user_dict.values()]):
-            return CheckResult.wrong('Please check all values are initially equal to 0')
-        elif not all([k.lower() in user_dict.keys() for k in friends]):
+        if not all([k.lower() in user_dict.keys() for k in friends]):
             return CheckResult.wrong('Please check all friends are in dictionary keys')
+
+        if luckypick:
+            if user_dict[luckyname] != 0:
+                return CheckResult.wrong("Bill value for lucky person should be 0")
+            if math.ceil(bill) != float(total) and math.floor(bill) != float(total):
+                return CheckResult.wrong("Please update dictionary with correct split values")
+            elif round(total / (num - 1), 2) not in bill_list:
+                return CheckResult.wrong("Please round off split values to two decimal places")
+        else:
+            if math.ceil(bill) != float(total) and math.floor(bill) != float(total):
+                return CheckResult.wrong("Please update dictionary with correct split values")
+            if bill_list[0] != round(total / num, 2):
+                return CheckResult.wrong("Please round off split values to two decimal places")
         return CheckResult.correct()
 
 
